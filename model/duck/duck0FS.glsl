373b78dda6d8098eb9574c341fd90f2fa66263ff
Fabrice Robinet <fabricerobinet@me.com>
2013-12-06 17:49:31 -0800

    fix a few regressions

--- model/duck/duck0FS.glsl
+++ model/duck/duck0FS.glsl
@@ -6,12 +6,14 @@ uniform float u_shininess;
 uniform vec4 u_ambient;
 varying vec2 v_texcoord0;
 uniform sampler2D u_diffuse;
+uniform vec4 u_emission;
 uniform vec4 u_specular;
 void main(void) {
 vec3 normal = normalize(v_normal);
 vec4 color = vec4(0., 0., 0., 0.);
 vec4 diffuse = vec4(0., 0., 0., 1.);
 vec3 diffuseLight = vec3(0., 0., 0.);
+vec4 emission;
 vec4 ambient;
 vec4 specular;
 vec3 specularLight = vec3(0., 0., 0.);
@@ -27,11 +29,13 @@ diffuseLight += u_light0Color * diffuseIntensity;
 }
 ambient = u_ambient;
 diffuse = texture2D(u_diffuse, v_texcoord0);
+emission = u_emission;
 specular = u_specular;
 specular.xyz *= specularLight;
 color.xyz += specular.xyz;
 diffuse.xyz *= diffuseLight;
 color.xyz += diffuse.xyz;
+color.xyz += emission.xyz;
 color = vec4(color.rgb * diffuse.a, diffuse.a);
 gl_FragColor = color;
 }
